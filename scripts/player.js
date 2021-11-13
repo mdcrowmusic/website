eea3db540d159bafc26bf1d0595dad8134f7009f
mdcrowmusic <49581667+mdcrowmusic@users.noreply.github.com>
2021-11-12 23:32:55 -0600

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -36,22 +36,3 @@ widget.getCurrentSound(function(currentSound) {
 });
 
 
-function updateBar() {
-var canvasWidth = 500
-var audioEl = document.getElementById("player")
-var canvas = document.getElementById("progress").getContext('2d')
-var num = 1
-
-  canvas.clearRect(0, 0, canvasWidth, 50)
-  canvas.fillStyle = "#f8f8ff";
-  canvas.fillRect(0, 0, canvasWidth, 50)
-
-  var currentTime = audioEl.currentTime;
-  var duration = audioEl.duration;
-
-
-  var percentage = currentTime / duration
-  var progress = (canvasWidth * percentage)
-  canvas.fillStyle = "rgb(0,85,120)"
-  canvas.fillRect(0, 0, progress, 50)
-};
