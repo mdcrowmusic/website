6c960e5583ecabaf00a2ef53da81e6aa28c38ed4
mdcrowmusic <49581667+mdcrowmusic@users.noreply.github.com>
2021-11-13 00:24:04 -0600

    Update musicplayer.js

--- scripts/musicplayer.js
+++ scripts/musicplayer.js
@@ -1,7 +1,7 @@
 var iframeElement   = document.querySelector('iframe');
 var iframeElementID = iframeElement.id;
 var widget         = SC.Widget(iframeElement);
-var x = document.getElementById("play");
+
 
 widget.bind(SC.Widget.Events.FINISH,function(){
   widget.getCurrentSound(function(currentSound) {
