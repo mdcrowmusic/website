5eff04e12325c6a96e8db7bbbabd5d710df8ec9b
mdcrowmusic <49581667+mdcrowmusic@users.noreply.github.com>
2021-11-12 23:40:27 -0600

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -1,12 +1,13 @@
 
-// Best practice stuff
 var iframeElement   = document.querySelector('iframe');
 var iframeElementID = iframeElement.id;
 var widget         = SC.Widget(iframeElement);
 var x = document.getElementById("play");
 
 
-
+function play(){
+widget.toggle();
+};
 
 widget.bind(SC.Widget.Events.READY, function() {
 widget.getCurrentSound(function(currentSound) {
