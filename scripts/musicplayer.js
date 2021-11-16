b4197c85270e7ebcabe15aeee1f58c6bb72e3a6c
mdcrowmusic <49581667+mdcrowmusic@users.noreply.github.com>
2021-11-15 21:11:48 -0600

    Update musicplayer.js

--- scripts/musicplayer.js
+++ scripts/musicplayer.js
@@ -1,66 +1,41 @@
-var iframeElement   = document.querySelector('iframe');
-var iframeElementID = iframeElement.id;
-var widget         = SC.Widget(iframeElement);
 
-
-widget.bind(SC.Widget.Events.FINISH,function(){
-  widget.getCurrentSound(function(currentSound) {
- document.getElementById("current").innerHTML = currentSound.title;
-});
-});
-
-function play(){ 
-  var x = document.getElementById("play");
-  if (x.innerHTML === "▶") {
-    x.innerHTML = "||";
-  } else {x.innerHTML = "▶";
-         }
-  widget.toggle();
-};
-
-function next(){
-  x.innerHTML = "||";
-  widget.next();
-  widget.seekTo(0);
-widget.getCurrentSound(function(currentSound) {
- document.getElementById("current").innerHTML = currentSound.title;
-});
-};  
-                       
-function prev(){
-  x.innerHTML = "||";
-  widget.prev();
-  widget.seekTo(0);
-widget.getCurrentSound(function(currentSound) {
- document.getElementById("current").innerHTML = currentSound.title;
-});
-}; 
+var iframeElement = document.querySelector('iframe');
+var widget = SC.Widget(iframeElement);
 
 widget.bind(SC.Widget.Events.READY, function() {
 widget.getCurrentSound(function(currentSound) {
- document.getElementById("current").innerHTML = currentSound.title;
-       
-  widget.getSounds(function(tracks){
-    for (var i in tracks)
-      {
-    
-       $('#tracklist').append("<li class='track-item' id='" + i + "'" + ">" + tracks[i].title + "</li>");
-    
-     $(".track-item").click(function(){
-       var s = this.id
-       widget.seekTo(0);
-    widget.skip(s);       
-      x.innerHTML = "||" ;     widget.getCurrentSound(function(currentSound) {
- document.getElementById("current").innerHTML = currentSound.title;
+ document.getElementById("currentTrack").innerHTML = currentSound.title;
+
+ widget.getSounds(function(tracks){
+   for (var i in tracks)
+     {
+
+      $('#tracklist').append("<li class='track-item' id='" + i + "'" + ">" + tracks[i].title + "</li>");
+
+    $(".track-item").click(function(){
+      var s = this.id
+      widget.seekTo(0);
+   widget.skip(s);
+     x.innerHTML = "||" ;     widget.getCurrentSound(function(currentSound) {
+document.getElementById("currentTrack").innerHTML = currentSound.title;
 });
-       
-  });
-    
-   };
- 
-  });
-  
-});
-  });
 
 
+ });
+
+  };
+
+ });
+
+});
+});
+
+
+function play(){
+  var x = document.getElementById('play')
+  if (x.innerHTML === "▶") {
+    x.innerHTML = "||";
+  } else {x.innerHTML = "▶";
+};
+  widget.toggle();
+};
