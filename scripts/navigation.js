7e3dacb7641c0432ddb842bcefde21556eedbcf7
mdcrowmusic <49581667+mdcrowmusic@users.noreply.github.com>
2021-12-24 00:06:00 -0600

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,7 +3,8 @@ var nav=$("#navigation .row");
 
 $("#siteTitle").click(function(){
    
-    $("#expander").animate({height:'50px'},"slow");  
+    $(".expander").animate({height:'8px'},"slow"); 
+   $(".row").css("{-webkit-box-shadow": "0px 1px 5px RGBa(0,85,120,0.5)"});
    $("#about").css('color', '');
    $("#music").css('color', '');
    $("#tracklist").hide();
@@ -12,7 +13,8 @@ $("#siteTitle").click(function(){
 
 $("#about").click(function(){
 
-   $("#expander").animate({height:'100%'},"slow");
+   $(".expander").animate({height:'100%'},"slow");
+   $(".row").css("{-webkit-box-shadow": "0px 0px 0px"});
 $("#aboutDiv").delay(500).fadeIn();
 $("#about").css('color','RGB(0,85,120)');
 $("#music").css('color', '');
@@ -20,7 +22,7 @@ $("#tracklist").hide();
 });
    
    $("#aboutMobile").click(function(){
-   $("#expander").animate({height:'100%'},"slow");
+   $(".expander").animate({height:'100%'},"slow");
 $("#aboutDiv").delay(500).fadeIn();
 $("#about").css('color','RGB(0,85,120)');
 $("#music").css('color', '');
@@ -50,7 +52,7 @@ $(this).attr("src", "../assets/img/yt_hover.png");
 $("#showTracklist").click(function(){
 $("#tracklist").delay(700).fadeIn();
 
-$("#expander").animate({height:'200px'},"slow");
+$(".expander").animate({height:'200px'},"slow");
 $("#about").css('color', '');
 $("#music").css('color', '');
 $("#showTracklist").delay(700).hide(0);
@@ -61,7 +63,7 @@ $("#aboutDiv").hide();
 
 $("#hideTracklist").click(function(){
   $("#tracklist").delay(1).hide(0);
-   $("#expander").animate({height:'50px'},"slow");
+   $(".expander").animate({height:'8px'},"slow");
   $("#showTracklist").delay(700).show(0);
   $("#hideTracklist").delay(700).hide(0);
 });
